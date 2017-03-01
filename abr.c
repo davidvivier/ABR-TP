3bfb98ba5058d1b1e0533532d1f7272286596939
David Vivier <contact@davidvivier.fr>
2017-03-01 16:03:31 +0100

    Create functions

--- abr.c
+++ abr.c
@@ -4,3 +4,20 @@
 
 #include <stdio.h>
 
+
+void ajout(Noeud **racine, int valeur) {
+	
+}
+
+void parcours_GRD(Noeud *racine) {
+
+}
+
+void parcours_RGD(Noeud *racine) {
+
+}
+
+void supression(Noeud **racine, int valeur) {
+
+}
+
--- abr.h
+++ abr.h
@@ -2,11 +2,22 @@
 #ifndef INCLUDE_ABR_H
 #define INCLUDE_ABR_H
 
-typedef struct Noeud Noeud
+typedef struct Noeud
 {
 	int val;
-	Noeud* gauche;
-	Noeud* droite;
-};
+	struct Noeud* gauche;
+	struct Noeud* droite;
+} Noeud;
+
+void ajout(Noeud **racine, int valeur);
+
+void parcours_GRD(Noeud *racine);
+
+void parcours_RGD(Noeud *racine);
+
+//void insertion_iterative(Noeud **racine, int valeur);
+
+void supression(Noeud **racine, int valeur);
+
 
 #endif
